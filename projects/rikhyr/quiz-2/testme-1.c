f0fed1ebfe8c226d8e7531b3f64681958708666a
Raj Rikhy <rajrikhy@Rajs-MacBook-Air.local>
2017-07-23 19:39:14 -0700

    quiz 2

---
+++ projects/rikhyr/quiz-2/testme-1.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+
+char inputChar()
+{
+    // TODO: rewrite this function
+const char chars[33]={'[','(','{',' ','}',')',']','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    return (chars[rand()%33]);
+}
+
+char *inputString()
+{
+    // TODO: rewrite this function
+const char *states[15]={"init","prepare","start","read","check","compute","analyze","printout","finalize","reset","update","downgrade","wait","listen","exit"};
+    return (states[(rand()%15)]);
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' ' && state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
